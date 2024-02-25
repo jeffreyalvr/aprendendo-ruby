39d679e26e1d7e31063a0ff5173f33b3e2b62593
Jeffrey Ramo <matheusunitt@gmail.com>
2024-02-25 14:29:41 -0300

    Adiciona casting_de_tipos e string_interpolation

---
+++ casting_de_tipos.rb
@@ -0,0 +1,5 @@
+id = 3484334
+temperatura = 23.4
+
+puts "O id buscado é: " + id.to_s
+puts "Está fazendo #{temperatura.to_i}"
---
+++ string_interpolation.rb
@@ -0,0 +1,4 @@
+nome = "jeffrey"
+idade = 28
+
+puts "Olá, meu nome é #{nome} e possuo #{idade} anos!"
